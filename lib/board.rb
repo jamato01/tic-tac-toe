b55bb5eaa80561d43eec8c8046c50d2b995f2882
jamato01 <jarrett.m.torres@gmail.com>
2025-09-11 11:38:04 -0400

    Add to_s definition

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,12 @@
 class Board
-  
+  attr_reader :board
   def initialize
     @board = ['   |   |   ', '-----------', '   |   |   ', '-----------', '   |   |   ']
     @board_positions = Array.new(3) { Array.new(3) }
   end 
+
+  def to_s
+    "#{board[0]}\n#{board[1]}\n#{board[2]}\n#{board[3]}\n#{board[4]}"
+  end
+
 end

50b62aad436d4f579963f2d448ace2c51803c07d
jamato01 <jarrett.m.torres@gmail.com>
2025-09-12 12:18:22 -0400

    Add number positions to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,10 @@
 class Board
   attr_reader :board
+  WINS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
   def initialize
-    @board = ['   |   |   ', '-----------', '   |   |   ', '-----------', '   |   |   ']
-    @board_positions = Array.new(3) { Array.new(3) }
+    @board = [' 1 | 2 | 3 ', '-----------', ' 4 | 5 | 6 ', '-----------', ' 7 | 8 | 9 ']
+    @board_positions = Array.new(9)
   end 
 
   def to_s
